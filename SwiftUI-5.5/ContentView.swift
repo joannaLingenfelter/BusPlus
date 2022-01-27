6d4f6ade43faeb633040b053cd304dacc52cf144
Jo Lingenfelter <lingenfelter.joanna@gmail.com>
2022-01-27 14:09:35 -0500

    Initial Commit

---
+++ SwiftUI-5.5/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI-5.5
+//
+//  Created by Joanna LINGENFELTER on 1/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
